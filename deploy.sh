e8c31b903c7f290d0ec86811b0c906e05ef3fb67
SakshiDhiman <49397348+SakshiDhiman@users.noreply.github.com>
2022-12-15 14:02:06 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ssh ubuntu@10.0.10.250 <<EOF 
+sudo su -
+cd /var/www/html/
+pm2 status
+pm2 delete react-site
+pm2 status
+rm -rf react_notes_app
+git clone  -b $1
+cd react_notes_app
+pwd
+npm install
+npm run build
+pm2 start npm --name "react-site" -- start "--port 3000"
+pm2 startup
+EOF
