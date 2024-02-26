f275a26c560f8b971720e087dee6d193fe8251a7
SakshiDhiman <49397348+SakshiDhiman@users.noreply.github.com>
2024-02-26 20:42:46 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -8,7 +8,7 @@ $localRepoPath = "C:\cftest"
 cd $localRepoPath
 
 # Pull the latest changes from the Bitbucket repository
-git pull https://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@github.com/SakshiDhiman/cftest.git
+git pull origin main
 
 # # Backup current deployment
 # Copy-Item -Path $deployPath -Destination $backupPath -Recurse -Force
